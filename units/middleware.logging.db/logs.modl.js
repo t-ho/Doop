094d043d3f6aac256b9bbc0c1ef82cddb0aa6a9e
t-ho <toan.hmt@gmail.com>
2018-05-11 16:54:57 +1000

    Remove angularJs

--- units/middleware.logging.db/logs.modl.js
+++
@@ -1,7 +0,0 @@
-angular
-	.module('app')
-	.factory('Logs', function($resource) {
-		return $resource('/api/logs/:id', {}, {
-			create: {url: '/api/logs/:model/:docId', method: 'POST'},
-		});
-	})
